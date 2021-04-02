09fadebdea2f6aa55a1afdd3e894ad25a45937eb
Ditipriya Seal <67802174+Ditipriya5678@users.noreply.github.com>
2021-04-02 13:52:48 +0530

    Created sWAP_cASE.py

---
+++ sWAP_cASE.py
@@ -0,0 +1,12 @@
+def swap_case(s):
+    result= ""
+    for letter in s:
+        if letter== letter.upper():
+            result+=letter.lower()
+        else:
+            result+=letter.upper()
+    return result
+    
+s = input()
+res = swap_case(s)
+print(res)
